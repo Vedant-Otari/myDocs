c05ff0f58114b18a67bd235dc7a45b67f339b6e2
Vedant Otari <vedantmotari24@gmail.com>
2023-11-18 21:56:44 +0530

    Socket.io added.

--- my-docs-client/src/TextEditor.js
+++ my-docs-client/src/TextEditor.js
@@ -1,29 +1,68 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import "quill/dist/quill.snow.css";
 import Quill from "quill";
+import { io } from "socket.io-client";
 
 const TOOLBAR_OPTIONS = [
-  ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
-  ['image','blockquote', 'code-block'],           // custom button values
-  [{ 'list': 'ordered'}, { 'list': 'bullet' }],
-  [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
-  [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
-  [{ 'direction': 'rtl' }],                         // text direction
-  [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
-  [{ 'header': [1, 2, 3, 4, 5, 6,7,8, false] }],
-  [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
-  [{ 'font': [] }],
-  [{ 'align': [] }],
-  ['clean']
+  ["bold", "italic", "underline", "strike"], // toggled buttons
+  ["image", "blockquote", "code-block"], // custom button values
+  [{ list: "ordered" }, { list: "bullet" }],
+  [{ script: "sub" }, { script: "super" }], // superscript/subscript
+  [{ indent: "-1" }, { indent: "+1" }], // outdent/indent
+  [{ direction: "rtl" }], // text direction
+  [{ size: ["small", false, "large", "huge"] }], // custom dropdown
+  [{ header: [1, 2, 3, 4, 5, 6, 7, 8, false] }],
+  [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+  [{ font: [] }],
+  [{ align: [] }],
+  ["clean"],
 ];
 
 export default function TextEditor() {
+  const [socket, setSocket] = useState();
+  const [quill, setQuill] = useState();
+  useEffect(() => {
+    const s = io("http://localhost:3001");
+    setSocket(s);
+    return () => {
+      s.disconneect();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (socket == null || quill == null) return;
+    const handler = (delta) => {
+      quill.updateContents(delta);
+    };
+    socket.on("receive-changes", handler);
+
+    return () => {
+      socket.off("receive-changes", handler);
+    };
+  }, [socket, quill]);
+
+  useEffect(() => {
+    if (socket == null || quill == null) return;
+    const handler = (delta, oldDelta, source) => {
+      if (source !== "user") return;
+      socket.emit("send-changes", delta);
+    };
+    quill.on("text-change", handler);
+    return () => {
+      quill.off("text-change", handler);
+    };
+  }, [socket, quill]);
+
   const wrapperRef = useCallback((wrapper) => {
-    if(wrapper==null)return
-    wrapperRef.innerHTML = ""
+    if (wrapper == null) return;
+    wrapperRef.innerHTML = "";
     const editor = document.createElement("div");
     wrapper.append(editor);
-    new Quill(editor, { theme: "snow" ,modules:{toolbar:TOOLBAR_OPTIONS}});
+    const q = new Quill(editor, {
+      theme: "snow",
+      modules: { toolbar: TOOLBAR_OPTIONS },
+    });
+    setQuill(q);
   }, []);
   return <div className="container" ref={wrapperRef}></div>;
 }
